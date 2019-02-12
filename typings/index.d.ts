291666c161a594e2b19f4ea94c7fdec3a57a5af5
andrew-polk <andrew_polk@sil.org>
2019-02-12 10:08:44 -0700

    Fix audio playback on Android 4.4 (BL-6888)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
+
+// Allows for polyfill in pagePlayer/app.ts
+interface Array<T> {
+  find(predicate: (search: T) => boolean): T;
+}
